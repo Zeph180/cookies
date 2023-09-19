ee9e914d2b2f225661bf417f0bb1ae403e0489ed
zeph180 <zephrichards1@gmail.com>
2023-09-19 11:08:48 +0300

    Add project files.

---
+++ cookies/HomePage.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="cookies.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Welcome:   <span id="usrName" runat="server"></span></p>
+            <p>You last visited: <span id="lstVst" runat="server"></span></p>
+
+            <p>
+                <asp:Label ID="Label2" runat="server"></asp:Label>
+            </p>
+            <hr />
+
+            <asp:Label Text="Enter your name:" runat="server"></asp:Label>
+            <br />
+            <asp:TextBox ID="nameInput" runat="server"></asp:TextBox>
+            <br />
+
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Save" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ cookies/HomePage.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace cookies
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //check if username cookie is available
+            if (Request.Cookies["UName"] != null)
+            {
+                usrName.InnerText = Request.Cookies["UName"].Value;
+            }
+            else
+            {
+                usrName.InnerText = "Guest";
+            }
+
+            //check if time of last visit exists
+            if (Request.Cookies["LVisit"] != null)
+            {
+                lstVst.InnerText = Request.Cookies["LVisit"].Value;
+            } 
+            else
+            {
+                lstVst.InnerText = "Never visited";
+            }
+
+            if (!IsPostBack)
+            {
+                //set last visit  when user opens or closes the page
+                HttpCookie cookie = new HttpCookie("LVisit");
+
+                //Set cookie value
+                cookie.Value = DateTime.Now.ToString();
+
+                //make the cookie persistent
+                cookie.Expires = DateTime.Now.AddDays(1);
+
+                //push cookie to client computer
+                Response.Cookies.Add(cookie);
+
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("UName");
+
+            //setting cookie value to texbox input
+            cookie.Value = nameInput.Text;
+
+            //make the cookie persistent
+            cookie.Expires = DateTime.Now.AddDays(1);
+            //push cookie to client computer
+            Response.Cookies.Add(cookie);
+
+            usrName.InnerText = nameInput.Text;
+        }
+    }
+}
